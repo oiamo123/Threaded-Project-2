b26616d9af21731ceea2aa10e0415b27fca660ef
Skye <dillonleewaddell@gmail.com>
2024-07-24 15:37:27 -0600

    Supplier Contact Design

--- ThreadedProject2/ThreadedProject2/AddEditItems/AddEditSupplierContact.Designer.cs
+++ ThreadedProject2/ThreadedProject2/AddEditItems/AddEditSupplierContact.Designer.cs
@@ -28,12 +28,390 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddEditSupplierContact";
+            txtSCID = new TextBox();
+            lblSupplierContactID = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtSCFirstName = new TextBox();
+            label4 = new Label();
+            txtSCLastName = new TextBox();
+            label5 = new Label();
+            txtSCCompany = new TextBox();
+            label6 = new Label();
+            txtSCAddress = new TextBox();
+            label7 = new Label();
+            txtSCCity = new TextBox();
+            label8 = new Label();
+            txtSCProvince = new TextBox();
+            label9 = new Label();
+            txtSCPostalCode = new TextBox();
+            label10 = new Label();
+            txtSCCountry = new TextBox();
+            label11 = new Label();
+            txtSCBusinessPhone = new TextBox();
+            label12 = new Label();
+            txtSCEmail = new TextBox();
+            label13 = new Label();
+            txtSCFax = new TextBox();
+            label14 = new Label();
+            txtSCURL = new TextBox();
+            btnAddEdit = new Button();
+            txtSCSupplierID = new TextBox();
+            label15 = new Label();
+            label16 = new Label();
+            comboAffilation = new ComboBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtSCID
+            // 
+            txtSCID.Location = new Point(12, 83);
+            txtSCID.Name = "txtSCID";
+            txtSCID.Size = new Size(329, 27);
+            txtSCID.TabIndex = 0;
+            // 
+            // lblSupplierContactID
+            // 
+            lblSupplierContactID.AutoSize = true;
+            lblSupplierContactID.Location = new Point(12, 60);
+            lblSupplierContactID.Name = "lblSupplierContactID";
+            lblSupplierContactID.Size = new Size(138, 20);
+            lblSupplierContactID.TabIndex = 1;
+            lblSupplierContactID.Text = "Supplier Contact ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 7);
+            label2.Name = "label2";
+            label2.Size = new Size(390, 41);
+            label2.TabIndex = 2;
+            label2.Text = "Add/Edit Supplier Contact";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 20);
+            label3.TabIndex = 4;
+            label3.Text = "First Name";
+            // 
+            // txtSCFirstName
+            // 
+            txtSCFirstName.Location = new Point(12, 143);
+            txtSCFirstName.Name = "txtSCFirstName";
+            txtSCFirstName.Size = new Size(329, 27);
+            txtSCFirstName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 186);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Last Name";
+            // 
+            // txtSCLastName
+            // 
+            txtSCLastName.Location = new Point(12, 209);
+            txtSCLastName.Name = "txtSCLastName";
+            txtSCLastName.Size = new Size(329, 27);
+            txtSCLastName.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 254);
+            label5.Name = "label5";
+            label5.Size = new Size(72, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Company";
+            // 
+            // txtSCCompany
+            // 
+            txtSCCompany.Location = new Point(12, 277);
+            txtSCCompany.Name = "txtSCCompany";
+            txtSCCompany.Size = new Size(329, 27);
+            txtSCCompany.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 326);
+            label6.Name = "label6";
+            label6.Size = new Size(62, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Address";
+            // 
+            // txtSCAddress
+            // 
+            txtSCAddress.Location = new Point(12, 349);
+            txtSCAddress.Name = "txtSCAddress";
+            txtSCAddress.Size = new Size(329, 27);
+            txtSCAddress.TabIndex = 9;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(371, 60);
+            label7.Name = "label7";
+            label7.Size = new Size(34, 20);
+            label7.TabIndex = 12;
+            label7.Text = "City";
+            // 
+            // txtSCCity
+            // 
+            txtSCCity.Location = new Point(371, 83);
+            txtSCCity.Name = "txtSCCity";
+            txtSCCity.Size = new Size(329, 27);
+            txtSCCity.TabIndex = 11;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(371, 120);
+            label8.Name = "label8";
+            label8.Size = new Size(65, 20);
+            label8.TabIndex = 14;
+            label8.Text = "Province";
+            // 
+            // txtSCProvince
+            // 
+            txtSCProvince.Location = new Point(371, 143);
+            txtSCProvince.Name = "txtSCProvince";
+            txtSCProvince.Size = new Size(329, 27);
+            txtSCProvince.TabIndex = 13;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(371, 186);
+            label9.Name = "label9";
+            label9.Size = new Size(87, 20);
+            label9.TabIndex = 16;
+            label9.Text = "Postal Code";
+            // 
+            // txtSCPostalCode
+            // 
+            txtSCPostalCode.Location = new Point(371, 209);
+            txtSCPostalCode.Name = "txtSCPostalCode";
+            txtSCPostalCode.Size = new Size(329, 27);
+            txtSCPostalCode.TabIndex = 15;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(371, 254);
+            label10.Name = "label10";
+            label10.Size = new Size(60, 20);
+            label10.TabIndex = 18;
+            label10.Text = "Country";
+            // 
+            // txtSCCountry
+            // 
+            txtSCCountry.Location = new Point(371, 277);
+            txtSCCountry.Name = "txtSCCountry";
+            txtSCCountry.Size = new Size(329, 27);
+            txtSCCountry.TabIndex = 17;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(371, 326);
+            label11.Name = "label11";
+            label11.Size = new Size(109, 20);
+            label11.TabIndex = 20;
+            label11.Text = "Business Phone";
+            // 
+            // txtSCBusinessPhone
+            // 
+            txtSCBusinessPhone.Location = new Point(371, 349);
+            txtSCBusinessPhone.Name = "txtSCBusinessPhone";
+            txtSCBusinessPhone.Size = new Size(329, 27);
+            txtSCBusinessPhone.TabIndex = 19;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(371, 399);
+            label12.Name = "label12";
+            label12.Size = new Size(46, 20);
+            label12.TabIndex = 24;
+            label12.Text = "Email";
+            // 
+            // txtSCEmail
+            // 
+            txtSCEmail.Location = new Point(371, 422);
+            txtSCEmail.Name = "txtSCEmail";
+            txtSCEmail.Size = new Size(329, 27);
+            txtSCEmail.TabIndex = 23;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(12, 399);
+            label13.Name = "label13";
+            label13.Size = new Size(30, 20);
+            label13.TabIndex = 22;
+            label13.Text = "Fax";
+            // 
+            // txtSCFax
+            // 
+            txtSCFax.Location = new Point(12, 422);
+            txtSCFax.Name = "txtSCFax";
+            txtSCFax.Size = new Size(329, 27);
+            txtSCFax.TabIndex = 21;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(12, 463);
+            label14.Name = "label14";
+            label14.Size = new Size(35, 20);
+            label14.TabIndex = 26;
+            label14.Text = "URL";
+            // 
+            // txtSCURL
+            // 
+            txtSCURL.Location = new Point(12, 486);
+            txtSCURL.Name = "txtSCURL";
+            txtSCURL.Size = new Size(329, 27);
+            txtSCURL.TabIndex = 25;
+            // 
+            // btnAddEdit
+            // 
+            btnAddEdit.Location = new Point(12, 602);
+            btnAddEdit.Name = "btnAddEdit";
+            btnAddEdit.Size = new Size(138, 39);
+            btnAddEdit.TabIndex = 27;
+            btnAddEdit.Text = "Add/Edit";
+            btnAddEdit.UseVisualStyleBackColor = true;
+            // 
+            // txtSCSupplierID
+            // 
+            txtSCSupplierID.Location = new Point(371, 486);
+            txtSCSupplierID.Name = "txtSCSupplierID";
+            txtSCSupplierID.Size = new Size(329, 27);
+            txtSCSupplierID.TabIndex = 29;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(12, 532);
+            label15.Name = "label15";
+            label15.Size = new Size(94, 20);
+            label15.TabIndex = 32;
+            label15.Text = "Affiliation ID";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(371, 463);
+            label16.Name = "label16";
+            label16.Size = new Size(83, 20);
+            label16.TabIndex = 33;
+            label16.Text = "Supplier ID";
+            // 
+            // comboAffilation
+            // 
+            comboAffilation.FormattingEnabled = true;
+            comboAffilation.Location = new Point(12, 555);
+            comboAffilation.Name = "comboAffilation";
+            comboAffilation.Size = new Size(329, 28);
+            comboAffilation.TabIndex = 34;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(156, 602);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 39);
+            button1.TabIndex = 35;
+            button1.Text = "Cancel";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // AddEditSupplierContact
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(714, 661);
+            Controls.Add(button1);
+            Controls.Add(comboAffilation);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(txtSCSupplierID);
+            Controls.Add(btnAddEdit);
+            Controls.Add(label14);
+            Controls.Add(txtSCURL);
+            Controls.Add(label12);
+            Controls.Add(txtSCEmail);
+            Controls.Add(label13);
+            Controls.Add(txtSCFax);
+            Controls.Add(label11);
+            Controls.Add(txtSCBusinessPhone);
+            Controls.Add(label10);
+            Controls.Add(txtSCCountry);
+            Controls.Add(label9);
+            Controls.Add(txtSCPostalCode);
+            Controls.Add(label8);
+            Controls.Add(txtSCProvince);
+            Controls.Add(label7);
+            Controls.Add(txtSCCity);
+            Controls.Add(label6);
+            Controls.Add(txtSCAddress);
+            Controls.Add(label5);
+            Controls.Add(txtSCCompany);
+            Controls.Add(label4);
+            Controls.Add(txtSCLastName);
+            Controls.Add(label3);
+            Controls.Add(txtSCFirstName);
+            Controls.Add(label2);
+            Controls.Add(lblSupplierContactID);
+            Controls.Add(txtSCID);
+            Name = "AddEditSupplierContact";
+            Text = "AddEditSupplierContact";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtSCID;
+        private Label lblSupplierContactID;
+        private Label label2;
+        private Label label3;
+        private TextBox txtSCFirstName;
+        private Label label4;
+        private TextBox txtSCLastName;
+        private Label label5;
+        private TextBox txtSCCompany;
+        private Label label6;
+        private TextBox txtSCAddress;
+        private Label label7;
+        private TextBox txtSCCity;
+        private Label label8;
+        private TextBox txtSCProvince;
+        private Label label9;
+        private TextBox txtSCPostalCode;
+        private Label label10;
+        private TextBox txtSCCountry;
+        private Label label11;
+        private TextBox txtSCBusinessPhone;
+        private Label label12;
+        private TextBox txtSCEmail;
+        private Label label13;
+        private TextBox txtSCFax;
+        private Label label14;
+        private TextBox txtSCURL;
+        private Button btnAddEdit;
+        private Label label1;
+        private TextBox txt;
+        private TextBox txtSCSupplierID;
+        private Label label15;
+        private Label label16;
+        private ComboBox comboAffilation;
+        private Button button1;
     }
 }
--- ThreadedProject2/ThreadedProject2/AddEditItems/AddEditSupplierContact.cs
+++ ThreadedProject2/ThreadedProject2/AddEditItems/AddEditSupplierContact.cs
@@ -16,5 +16,6 @@ namespace ThreadedProject2
         {
             InitializeComponent();
         }
+
     }
 }
